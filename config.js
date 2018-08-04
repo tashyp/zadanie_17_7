4758e96159ac24f226cee1e985f6d3efc030611e
Przemek Saja <przemek.saja@gmail.com>
2018-08-04 11:09:54 +0200

    Add files with app

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '316791180839-i364397hmt809pmccvl44v6bd2nrd6jq.apps.googleusercontent.com ',
+	'GOOGLE_CLIENT_SECRET': '5TFsY0HsP7Em04HRznwIgyEq',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
